4172fc6be0c3ccadfe7e629bbcd226cfa40f4df8
Lameck Santos <lameck2@hotmail.com>
2024-11-08 21:42:57 -0300

    feat: Remove previous images, set wallpaper

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 import requests
 import random
-
+import os
 
 def has_class_preview(tag):
     return tag.has_attr('class') and tag['class'][0] == 'preview'
@@ -26,12 +26,22 @@ def get_image(image_page_url):
 
     return image_url
 
+def delete_previous_wallpapers():
+    os.system('rm -rf *.jpg *.png')
+
+def set_wallpaper(file_name):
+    file_location = f"{os.getcwd()}/{file_name}"
+    print(f"osascript -e 'tell application \"System Events\" to tell every desktop to set picture to \"{file_location}\"'")
+    os.system(f"osascript -e 'tell application \"System Events\" to tell every desktop to set picture to \"{file_location}\"'")
+
 def download_and_save_image(image_url):
     response = requests.get(image_url)
     file_name = image_url.split('/')[-1]
+    delete_previous_wallpapers()
 
     with open(file_name, 'wb') as file:
         file.write(response.content)
+        set_wallpaper(file_name)
 
 page_url = get_image_page_url()
 img_url = get_image(page_url)
